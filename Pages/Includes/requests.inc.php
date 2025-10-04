59ab3aefdff05015547f18f4d6c9918e3e4f1097
jbeau085 <jbeau085@mtroyal.ca>
2025-10-04 13:59:45 -0600

    Added link.db,requests.inc.php and statements.sql.

---
+++ Pages/Includes/requests.inc.php
@@ -0,0 +1,10 @@
+<!-- Requests from link.db
+ Use these commands to request data in the forms -->
+<?php
+// Define connString pointer to link.db
+define('connString','sqlite:./database/link.db');
+// connection 
+class linkDB {
+    private static $baseSQL = "SELECT * FROM "
+}
+?>
---
+++ Pages/Includes/statements.sql
@@ -0,0 +1,33 @@
+-- Create database
+/*
+CREATE TABLE activeSessions(
+    sessionID INT PRIMARY KEY NOT NULL,
+    groupID INT,
+    locationCode INT,
+    subject INT,
+    traffic INT,
+    description TEXT);
+*/
+/*
+CREATE TABLE groups(
+    groupID INT PRIMARY KEY NOT NULL,
+    groupName TEXT NOT NULL);
+*/
+/*
+CREATE TABLE groups(
+    groupID INT PRIMARY KEY NOT NULL,
+    groupName TEXT NOT NULL);
+    sqlite> .schema users
+    CREATE TABLE users(
+    userName TEXT PRIMARY KEY NOT NULL,
+    userPassword TEXT NOT NULL,
+    groupID INT,
+    userSubject TEXT,
+    userSkills TEXT,
+    currentTraffic INT,
+    currentLocation INT,
+    preferredLight INT,
+    preferredTime STRING);
+*/
+-- 
+
