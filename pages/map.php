cfead543655b58c4d9346ed0d5287d7d22ff14dc
Diesel Thomas <151065987+dthomas1202@users.noreply.github.com>
2025-10-04 20:46:58 -0600

    Working map

--- pages/map.php
+++ pages/map.php
@@ -1,14 +1,61 @@
+<?PHP
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php require("static/head.php");?>
-<link rel="stylesheet" href="./style.css">
+    <?php require("static/head.php");?>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster/dist/MarkerCluster.css"/>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster/dist/MarkerCluster.Default.css"/>
+
+    <link rel="stylesheet" href="css/map.css">
+    <link rel="stylesheet" href="css/common.css">
 </head>
 <body>
-<?php require("static/header.php");?>
-<main>
-<h1>Welcome to My Website</h1>
+    <?php require("static/header.php");?>
 
-</main>
+    <main>
+        <div id="sidebar">
+            <div id="sidebarInit">
+                <h3>Please Select a Session in the Map</h3>
+            </div>
+            <div id="sidebarCards">
+                <div class="sessionCard">
+                    <div class="trafficRed cardTraffic"></div>
+                    <p class="sessionSubject">Subject</p>
+                    <p class="sessionTime">10:00am to<br> 3:00pm</p>
+                    <p class="sessionUser">Username</p>
+                </div>
+                <div class="sessionCard">
+                    <div class="trafficYellow cardTraffic"></div>
+                    <p class="sessionSubject">Subject</p>
+                    <p class="sessionTime">10:00am to<br> 3:00pm</p>
+                    <p class="sessionUser">Username</p>
+                </div>
+                <div class="sessionCard">
+                    <div class="trafficGreen cardTraffic"></div>
+                    <p class="sessionSubject">Subject</p>
+                    <p class="sessionTime">10:00am to<br> 3:00pm</p>
+                    <p class="sessionUser">Username</p>
+                </div>
+            </div>
+            <div id="sidebarInfo">
+                <p>Username</p>
+                <p>Subject</p>
+                <p>Time</p>
+                <p><span class="trafficRed"></span> - Locked In</p>
+                <p>Description</p>
+                <button type="button" id="sessionJoin">Join</button>
+                <button type="button" id="sessionLeave">Leave</button>
+            </div>
+        </div>
+        <div id="map"></div>
+    </main>
+
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <script src="https://unpkg.com/leaflet.markercluster/dist/leaflet.markercluster.js"></script>
+    <script src="js/map.js"></script>
 </body>
 </html>
