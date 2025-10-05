c816a00801a9df8446b9ea9f5838ef1e7c1b8508
jbeau085 <jbeau085@mtroyal.ca>
2025-10-05 08:16:53 -0600

    fixed header left most item to redirect to index.php

--- pages/index.php
+++ pages/index.php
@@ -14,7 +14,7 @@
 <body>
     <header>
         <div class="leftHeader">
-            <a href="header.html" class="logo">Home
+            <a href="index.php" class="logo">Home
                 <!-- <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
                     <path
                         d="M34 17C34 26.3888 26.3888 34 17 34C7.61116 34 -8.20798e-07 26.3888 0 17C8.20799e-07 7.61116 7.61116 -8.20799e-07 17 0C26.3888 8.20799e-07 34 7.61116 34 17Z"
