bf7c92f96413b00a7dd472f2d64f7d0daa073c76
jbeau085 <jbeau085@mtroyal.ca>
2025-10-04 21:35:51 -0600

    edits to database, edits to session.php

--- pages/include/session.php
+++ pages/include/session.php
@@ -14,7 +14,7 @@
     }
     ?>
   */
-require_once __DIR__ . '../../database/linkDB.php';
+require_once __DIR__ . '../../../database/linkDB.php';
 session_start();
 if (! empty($_POST) ) {
     if (isset($_POST['username']) && isset($_POST['password'])) {
